3f5f6c596ee3ec00390a42ee166c09b79794f568
Vladislav Iurciuc <fletcher6847@gmail.com>
2018-04-28 22:45:05 +0300

    HW-day4

---
+++ src/Controller/JobController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Job;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JobController extends AbstractController
+{
+    /**
+     * List of all jobs
+     *
+     * @Route("/", name="job.list")
+     * @Method("GET")
+     * @return Response
+     * @throws \LogicException
+     */
+    public function listAction(): Response
+    {
+        $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
+
+        return $this->render('job/list.html.twig', [
+            'jobs' => $jobs,
+        ]);
+    }
+}
